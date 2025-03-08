4e37acefca3175e28028a1bd98425f51d0815fa3
jumashafara <jumashafara0@gmail.com>
2025-03-08 11:26:27 +0300

    fix: update navbar restriction

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -80,7 +80,8 @@ const HomePage: React.FC = () => {
         </div>
       </main>
       <footer className="bg-gray-800 w-full py-4 text-center text-white">
-        &copy; 2025 DATAIDEA Meet. All rights reserved.
+        &copy; 2025 EASI Meet. All rights reserved. Powered by 
+        {/* <a href="https://dataidea.org" className="text-[#DD8604] text-decoration-underline">DATAIDEA</a> */}
       </footer>
     </div>
   );
